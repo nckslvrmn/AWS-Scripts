410870097ebc7b9b6897469b765860a09d43b306
Nick Silverman <nckslvrmn@gmail.com>
2024-07-22 16:06:45 -0400

    add some flags to bucket size script for outputs and formatting

--- bucket_sizes/bucket_sizes.py
+++ bucket_sizes/bucket_sizes.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import argparse
 import csv
+import json
 import sys
 
 from datetime import datetime
@@ -26,23 +28,31 @@ STORAGE_TYPES = [
 ]
 
 
+def humanize_bytes(num, suffix="B"):
+    for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
+
+
 def construct_query(bucket_name):
     queries = []
     for stype in STORAGE_TYPES:
         queries.append(
             {
-                'Id': stype.lower(),
-                'MetricStat': {
-                    'Metric': {
-                        'Namespace': 'AWS/S3',
-                        'MetricName': 'BucketSizeBytes',
-                        'Dimensions': [
-                            {'Name': 'BucketName', 'Value': bucket_name},
-                            {'Name': 'StorageType', 'Value': stype},
+                "Id": stype.lower(),
+                "MetricStat": {
+                    "Metric": {
+                        "Namespace": "AWS/S3",
+                        "MetricName": "BucketSizeBytes",
+                        "Dimensions": [
+                            {"Name": "BucketName", "Value": bucket_name},
+                            {"Name": "StorageType", "Value": stype},
                         ],
                     },
-                    'Period': 86400,
-                    'Stat': 'Maximum',
+                    "Period": 86400,
+                    "Stat": "Maximum",
                 },
             }
         )
@@ -50,31 +60,51 @@ def construct_query(bucket_name):
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--bucket", default=None)
+    parser.add_argument("--humanize", action="store_true", default=False)
+    parser.add_argument("--json", action="store_true", default=False)
+    args = parser.parse_args()
+
     sess = boto3.session.Session()
-    s3 = sess.client('s3')
-    cw = sess.client('cloudwatch')
+    s3 = sess.client("s3")
+    cw = sess.client("cloudwatch")
     sizes = []
-    if len(sys.argv) > 1:
-        buckets = [sys.argv[1]]
-    else:
-        buckets = [bucket['Name'] for bucket in s3.list_buckets()['Buckets']]
+    buckets = (
+        [args.bucket]
+        if args.bucket is not None
+        else [bucket["Name"] for bucket in s3.list_buckets()["Buckets"]]
+    )
+
     for bucket in buckets:
-        if s3.head_bucket(Bucket=bucket)['ResponseMetadata']['HTTPHeaders']['x-amz-bucket-region'] != sess.region_name:
+        if (
+            s3.head_bucket(Bucket=bucket)["ResponseMetadata"]["HTTPHeaders"][
+                "x-amz-bucket-region"
+            ]
+            != sess.region_name
+        ):
             continue
-        bucket_sizes = {'Name': bucket}
+        bucket_sizes = {"Name": bucket}
         resp = cw.get_metric_data(
-            MetricDataQueries=construct_query(bucket), StartTime=(datetime.today() - timedelta(days=3)), EndTime=(datetime.today() - timedelta(days=2))
+            MetricDataQueries=construct_query(bucket),
+            StartTime=(datetime.today() - timedelta(days=3)),
+            EndTime=(datetime.today() - timedelta(days=2)),
         )
-        for metric in resp['MetricDataResults']:
-            if len(metric['Values']) > 0:
-                value = metric['Values'][0]
-            else:
-                value = 0
-            bucket_sizes[metric['Label']] = value
+        for metric in resp["MetricDataResults"]:
+            if len(metric["Values"]) > 0:
+                bucket_sizes[metric["Label"]] = (
+                    humanize_bytes(metric["Values"][0])
+                    if args.humanize
+                    else metric["Values"][0]
+                )
         sizes.append(bucket_sizes)
-    w = csv.DictWriter(sys.stdout, fieldnames=['Name'] + STORAGE_TYPES)
-    w.writeheader()
-    w.writerows(sizes)
+
+    if args.json:
+        print(json.dumps(sizes, indent=2))
+    else:
+        w = csv.DictWriter(sys.stdout, fieldnames=["Name"] + STORAGE_TYPES)
+        w.writeheader()
+        w.writerows(sizes)
 
 
 if __name__ == "__main__":
